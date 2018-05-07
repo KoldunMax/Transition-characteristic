87313facea8e66939b8d814497490f01ec2a4b6d
Maksym Koldun <koldun.maks@gmail.com>
2018-05-07 23:59:49 +0300

    update js, split model and PID controller

--- app/js/common.js
+++ app/js/common.js
@@ -102,8 +102,8 @@ $(function() {
 	var StopButton = document.getElementById("StopButton");
 	var UpButton = document.getElementById("UpButton");
 	var DownButton = document.getElementById("DownButton");	
-	/*var UpButtonEnter = document.getElementById("UpButtonEnter");
-	var DownButtonEnter = document.getElementById("DownButtonEnter");*/
+	var UpButtonEnter = document.getElementById("UpButtonEnter");
+	var DownButtonEnter = document.getElementById("DownButtonEnter");
 	var ClearButton = document.getElementById("ClearButton");
 	var NoiseButton = document.getElementById("NoiseButton");
 	var FastBuild = document.getElementById("FastBuild");
@@ -111,247 +111,126 @@ $(function() {
 	var valueOfU1 = document.getElementById("valueOfU1");
 	var valueOfLiquid = document.getElementById("valueOfLiquid");
 	var valueOfX = document.getElementById("valueOfX");
+	var checkbox = document.getElementById("checkbox");
+	var inputX = document.getElementById("inputX");
 
-	class ElementPID {
-
-		constructor(K, Td, Tu, X){
+	class Model {
+		constructor(K, T) {
 			this.K = K;
-			this.Tu = Tu;
-			this.Td = Td;
-			this.X = X;
-
-			this.Q_0 = 0;
-			this.Q_1 = 0;
-			this.Q_2 = 0;
-
-			this._Y = 0;
-			this._X = 0;
-			this._Tkv = 1;
-			this._time = 0;
-			this._timer;
-			this.e = [0];
-			this.U = 50;
-			this.U1 = 0;
-			this._Uvs = 0;
-			this._Xdif = 0;
+            this.T = T;
+            this._y = 0;
+            this._yprev = 0;
+            this._time = 0;
 		}
 
-		set setU(U){
-			if(U < 101 && U >= 0){
-				this.U = U;
-			} else {
-				console.log("Error, You cannot change U, more than unit of 100 or less than 0");
+		getY(U, U1) {
+			this._y = this.K * this._time * (U - U1) / this.T + this._yprev;
+			if(this._y < 0) {
+				this._y = 0;
 			}
+			this._yprev = this._y;
+
+			return this._y;
 		}
-
-		get getU(){
-			return this.U;
-		}
-
-		set setU1(U){
-			if(U < 101 && U >= 0){
-				this.U1 = U;
-			} else {
-				console.log("Error, You cannot change U, more than unit of 100 or less than 0");
-			}
-		}
-
-		get getU1(){
-			return this.U1;
-		}	
-
-		set setQ_0(obj) {
-			this.Q_0 = obj.K*(1 + obj.Td/this._Tkv);
-		} 	
-
-		get getQ_0() {
-			return this.Q_0;
-		}
-
-		set setQ_1(obj){
-			this.Q_1 = -obj.K*(1+ 2*(obj.Td/this._Tkv) -(this._Tkv/obj.Tu));
-		}
-
-		get getQ_1(){
-			return this.Q_1;
-		}
-
-		set setQ_2(obj){
-			this.Q_2 = obj.K*(obj.Td/this._Tkv);
-		}
-
-		get getQ_2(){
-			return this.Q_2;
-		}
-
-		set setE(obj){
-			this.e.push(obj.Yn - obj.X);
-		}
-
-		get getE(){
-			return this.e[this.e.length-1];
-		}
-
-		set setY(Y1){
-			this._Y = (this.K * this._Xdif * this._time + this.Tu * Y1) / (this.Tu + this._time); /*this.U*/;
-			if(this._Y  < 0){
-				this._Y = 0;
-			} 
-
-			valueOfLiquid.innerHTML = `<p><b>${this._Y} ml</b></p>`;
-		}
-
-		get getY(){
-			return this._Y;
-		}
-
-		set setUvs(obj) {
-
-			obj.Yn_1 == undefined ? obj.Yn_1 = 0 : '';
-			obj.E_k == undefined ? obj.E_k = 0 : '';
-			obj.E_k_1 == undefined ? obj.E_k_1 = 0 : '';
-			obj.E_k_2 == undefined ? obj.E_k_2 = 0 : '';
-			obj.Q_0 == Infinity ||  obj.Q_0 == -Infinity ? obj.Q_0 = 0 : '';
-			obj.Q_1 == Infinity ||  obj.Q_1 == -Infinity ? obj.Q_1 = 0 : '';
-			obj.Q_2 == Infinity ||  obj.Q_2 == -Infinity ? obj.Q_2 = 0 : '';
-
-			this._Uvs = obj.Yn_1+obj.Q_0*obj.E_k + obj.Q_1*obj.E_k_1+ obj.Q_2*obj.E_k_2 ;/*+ this.Noise();*/
-			valueOfU.innerHTML = `<p><b>U = ${newTank.getU}</b></p>`;
-			
-			if(this._Y > this.X + 0.25) {
-				this.setU = this.getU - 1;
-			} else if(this._Y < this.X - 0.25){
-				this.setU = this.getU + 1;
-			}
-			this._Xdif = this.U - this._Uvs;
-		}
-
-		get getUvs() {
-			return this._Uvs;
-		}
-
-		set setX(value){
-			return this._X = value
-		}
-
-		get getX(){
-			return this._X;
-		}
-
-		set settingX(X){
-			this.X = X;
-		}
-		get gettingX(){
-			return this.X;
-		}
-
-
-
-		// `creatingChart` create chart with using library Chart.js, `makechart` launch timer and create chart dynamically depend from real time
-
-		creatingChart(){
-		   this.setQ_0 = {K: this.K, Td: this.Td};
-		   this.setQ_1 = {K: this.K, Td: this.Td, Tu: this.Tu};
-		   this.setQ_2 = {K: this.K, Td: this.Td};
-		   this.setUvs = {Yn_1: chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1], Q_0: this.getQ_0, E_k: this.getE, Q_1: this.getQ_1, E_k_1: this.e[this.e.length-2], Q_2: this.getQ_2, E_k_2: this.e[this.e.length-3]};
-		   this.setY = chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1];
-/*		   this.setY = {Yn_1: chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1], Q_0: this.getQ_0, E_k: this.getE, Q_1: this.getQ_1, E_k_1: this.e[this.e.length-2], Q_2: this.getQ_2, E_k_2: this.e[this.e.length-3]};*/
-   		   this.setE = {Yn: this.getY, X: this.X};
-		   chart.data.datasets[0].data.push(this.getY);   // set new Point at the Y`s axe
-		   // ------ ------ ------- --------- ------------ ------------- -------------- ----------    //
-		   this.setX = chart.data.labels[chart.data.labels.length-1] + 1;
-		   chart.data.labels.push(this.getX);								
-		   chart.update();  
-		   this._time = this._time + 100;
-		}
-
-		startTimer(){
-			if(this._timer == undefined && chart.data.labels[chart.data.labels.length-1] < 45){
-				this._timer = setInterval(() => {
-					this.creatingChart();
-				}, 200)
-			} else {
-				this.stopTimer();
-				alert("Error");
-			}	
-		}
-
-		stopTimer(){
-			if(this._timer != undefined){
-				clearInterval(this._timer); 																	// if timer exist i stop it
-				this._timer = undefined;
-			} else {
-				console.log("timer is undefined");
-			}
-		}
-
-		clearChart(){
-			this.stopTimer();
-			this._time = 0;
-			this.setX = 0;
-			this.setY = {U: this.U, K: this.K, Yn_1: 0};
-			chart.data.datasets[0].data.splice(1, chart.data.datasets[0].data.length);
-			chart.data.labels.splice(1, chart.data.labels.length);
-			valueOfLiquid.innerHTML = `<p><b>${chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1]} ml</b></p>`;
-		    chart.update();
-		}
-
-		Noise(){
-			if(NoiseButton.defaultValue == `Noise On`){
-				return ElementPID.randomNumber(-.05, .05);
-			} else {
-				return 0;
-			}
-		}
-
-		static randomNumber(min, max){
-			var rand = min - 0.5 + Math.random() * (max - min + 1)
-		    rand = Math.round(rand);
-		    return rand;
-		}
-
-		fastBuild(){
-			if(chart.data.labels[chart.data.labels.length-1] == 0){
-
-/*				if(this.U > this.U1) {
-					console.log("s1s");
-					console.log(chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1]);
-					while(chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1] > 0){
-						this.creatingChart()
-					}
-				} else {
-					console.log("ss");*/
-					while(chart.data.labels.length < 100 && chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1] < 10000) {
-						this.creatingChart()
-					}
-				//}
-			}
-		}
-
 	}
 
-	// Creating new object with name newTank
+	class PIDcontroler extends Model {
+		constructor(Kp, Tu, Td){
+			super(Kp, Tu);
+			this.Td = Td;
+			this.u = 0;
+			this.u_1 = 0;
+			this.e = 0;
+			this.e_1 = 0;
+			this.e_2 = 0;
+		}
 
-	var newTank = new ElementPID(0.2, 0.6, 1.2, 6);
-	valueOfU.innerHTML = `<p><b>U = ${newTank.getU}</b></p>`;
-	valueOfU1.innerHTML = `<p><b>U1 = ${newTank.getU1}</b></p>`;
-	valueOfX.innerHTML = `<p><b>X = <input type="text" name="EnteringX" id='inputX' value=${newTank.gettingX}></b></p>`;
-	var inputX = document.getElementById("inputX");
-	valueOfLiquid.innerHTML = `<p><b>${chart.data.datasets[0].data[chart.data.datasets[0].data.length - 1]} ml</b></p>`;
-	var timer = '';   												// timer`ll use below
-	var timer1 = '';   												// timer`ll use below
+		getU(e) {
+            this.u = this.u_1+this.K*(1+this.Td/this._time)*e-this.K*(1+2*(this.Td/this._time)-(this._time/this.T))*this.e_1+this.K*(this.Td/this._time)*this.e_2;
+            this.u_1 = this.u;
+            this.e_2 = this.e_1;
+            this.e_1 = this.e;
 
-	// Buttons -----> They start functional of class`s methods
+            return this.u;
+		}
+	}
 
-	StartButton.onclick = () => { newTank.startTimer(); } 
 
-	StopButton.onclick = () => { newTank.stopTimer() }
+	var timerForChart = null;
+	var timer = null;  					// for buttons up and Down U, U1
+	var model = new Model(2.5, 9);
+	var modelPID = new PIDcontroler(2, 10, 0);
+	var y;
+	var e;
+	var u;
 
-	// UpButton.onclick = () => { newTank.setU = newTank.getU + 1; valueOfU.innerHTML = `<p><b>U = ${newTank.getU}</b></p>`; }
+	checkbox.onclick = () => {
+		if(checkbox.getAttribute("checked") == "checked"){
+			valueOfX.style.display = "none";
+			checkbox.setAttribute("checked", "")
+			UpButtonEnter.style.opacity = 1;
+			DownButtonEnter.style.opacity = 1;
+		}	else {
+			checkbox.setAttribute("checked", "checked");
+			valueOfX.style.display = "block";
+			UpButtonEnter.style.opacity = .2;
+			DownButtonEnter.style.opacity = .2;
+
+			modelPID._time += 1;
+			y = modelPID.getY(+valueOfU.innerText, +valueOfU1.innerText);
+		    e = y - (+inputX.value);
+		    u = modelPID.getU(e);
+		    valueOfU1.innerHTML = `<p><b>U1 = ${Math.round(u)}</b></p>`;
+			chart.data.labels.push(modelPID._time);	
+	 		chart.data.datasets[0].data.push(y); 
+			chart.update();
+		}
+	}
+
+
+	
+	
+	StartButton.addEventListener("click", () => {
+		if(checkbox.getAttribute("checked") == "checked"){
+			timerForChart = setInterval(() => {
+			y = modelPID.getY(+valueOfU.innerText, u);
+			e = y - (+inputX.value);
+		    u = modelPID.getU(e);
+		    valueOfU1.innerHTML = `<p><b>U1 = ${Math.round(u)}</b></p>`;
+		    modelPID._time += 1;
+			chart.data.labels.push(modelPID._time);	
+	 		chart.data.datasets[0].data.push(y); 
+			chart.update();
+			}, 50);
+		} else {
+			timerForChart = setInterval(() => {
+			model._time += 1;
+
+			chart.data.labels.push(model._time);	
+	 		chart.data.datasets[0].data.push(model.getY()); 
+			chart.update();
+			valueOfLiquid.innerHTML = `<p>${model.getY()}</p>`
+		}, 1000)
+		}
+	});
+
+
+
+	StopButton.onclick = function(){
+		if(timerForChart) {
+			clearInterval(timerForChart);
+			timerForChart = null;
+		} else {
+			alert(`timerForChart is already working`);
+		}
+	}	
+
 
 	UpButton.addEventListener("mousedown", () => {
 		timer = setInterval(() => {
-			newTank.setU = newTank.getU + 1; valueOfU.innerHTML = `<p><b>U = ${newTank.getU}</b></p>`;
+			let U = +valueOfU.innerText;
+			valueOfU.innerHTML = `<p><b>U = ${++U}</b></p>`;
+			valueOfU.innerText = U;
 		}, 50);
 	})
 
@@ -361,54 +240,17 @@ $(function() {
 
 	DownButton.addEventListener("mousedown", () => {
 		timer = setInterval(() => {
-			newTank.setU = newTank.getU - 1; valueOfU.innerHTML = `<p><b>U = ${newTank.getU}</b></p>`;
+			let U = +valueOfU.innerText;
+			valueOfU.innerHTML = `<p><b>U = ${--U}</b></p>`;
+			valueOfU.innerText = U;
 		}, 50);
 	});
 
 	DownButton.addEventListener("mouseup", () => {
 		timer != undefined ? clearInterval(timer) : console.log("timer doesn`t exist");
 	});	
-/*
-	inputX.addEventListener("change", () => {
-		alert(inputX.value);
-	});*/
-
-	inputX.addEventListener("keypress", () => {
-		setTimeout(() => {
-			newTank.settingX = +inputX.value;
-		}, 0);
-	});
-
-
-	UpButtonEnter.addEventListener("mousedown", () => {
-		timer1 = setInterval(() => {
-			newTank.setU1 = newTank.getU1 + 1; valueOfU1.innerHTML = `<p><b>U = ${newTank.getU1}</b></p>`;
-		}, 50);
-	})
-
-	UpButtonEnter.addEventListener("mouseup", () => {
-		timer1 != undefined ? clearInterval(timer1) : console.log("timer doesn`t exist");
-	});
-
-	DownButtonEnter.addEventListener("mousedown", () => {
-		timer1 = setInterval(() => {
-			newTank.setU1 = newTank.getU1 - 1; valueOfU1.innerHTML = `<p><b>U = ${newTank.getU1}</b></p>`;
-		}, 50);
-	});
-
-	DownButtonEnter.addEventListener("mouseup", () => {
-		timer1 != undefined ? clearInterval(timer1) : console.log("timer doesn`t exist");
-	});
-
-	// DownButton.onclick = () => {  newTank.setU = newTank.getU - 1; valueOfU.innerHTML = `<p><b>U = ${newTank.getU}</b></p>`; };
-
-	ClearButton.onclick = () => { newTank.clearChart(); }
-
-	NoiseButton.onclick = () => { 
-		NoiseButton.defaultValue = (NoiseButton.defaultValue == `Noise On`) ? `Noise Off` : `Noise On`;
-		newTank.Noise(); 
-	}
-
-	FastBuild.onclick = () => { newTank.fastBuild() }
 
 });
+
+
+
